9e51a34e8ce67458dc01f550e5a34158f57746af
@hexlet/cli <support@hexlet.io>
2023-05-19 18:28:09 +0300

    submit java-spring-ru/finite-state-machine

---
+++ java-spring-ru/finite-state-machine/src/main/java/exercise/model/Post.java
@@ -0,0 +1,42 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "posts")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String title;
+
+    @Lob
+    private String body;
+
+    private PostState state = PostState.CREATED;
+
+    // BEGIN
+    public Boolean publish() {
+        if (state == PostState.CREATED) {
+            state = PostState.PUBLISHED;
+            return true;
+        }
+        return false;
+    }
+
+    public Boolean archive() {
+        if (state == PostState.CREATED || state == PostState.PUBLISHED) {
+            state = PostState.ARCHIVED;
+            return true;
+        }
+        return false;
+    }
+    // END
+}
